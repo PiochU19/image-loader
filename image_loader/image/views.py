62155f4cae2ffdcdd001377345d68679bf4d2387
PiochU19 <792954018@wp.pl>
2021-05-18 13:29:21 +0200

    Custom Permissions
    * 'IsImageOwner returns True if logged user is owner of retrieved image

--- image_loader/image/views.py
+++ image_loader/image/views.py
@@ -2,6 +2,7 @@ from image_loader.image.models import MainImage
 from image_loader.image.serializers import ImageMainSerializer
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
+from image_loader.permissions import IsImageOwner
 
 
 class ImageMainViewSet(
@@ -14,8 +15,10 @@ class ImageMainViewSet(
     ViewSet with create, retrieve and list methods
     """
 
+    permission_classes = (IsImageOwner,)
     serializer_class = ImageMainSerializer
     queryset = MainImage.objects.all()
+    lookup_field = "image_name"
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
